edd85066611c0c1c5b5417e2254c273c7301b6e8
Ivan Kovach <eeiprex@gmail.com>
2023-08-03 22:41:19 +0300

    [Part 2]: Add project `3`
    Определяем максимум среди трех чисел.

---
+++ Part2/3/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double n1, n2, n3;
+
+    printf("Введите три числа: ");
+    scanf("%lf %lf %lf", &n1, &n2, &n3);
+
+    if (n1 >= n2 && n1 >= n3) printf("%.2f - максимум.\n", n1);
+
+    if (n2 >= n1 && n2 >= n3) printf("%.2f - максимум.\n", n2);
+
+    if (n3 >= n1 && n3 >= n2) printf("%.2f - максимум.\n", n3);
+}
